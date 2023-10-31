af73e5056f43ea023604dab486c30f5da98165a6
FernandoAlfonso07 <148348192+FernandoAlfonso07@users.noreply.github.com>
2023-10-31 09:17:29 -0600

    Add files via upload

--- archivo1.py
+++ archivo1.py
@@ -1,4 +1,4 @@
-#VERSION 7_Distancia - AUTOR : Fernando Alfonso
+#VERSION 8_ajedrez - AUTOR : Fernando Alfonso
 
 x1 = int(input("Ingrese columna 1 = "))
 x2 = int(input("Ingrese fila 1 = "))
@@ -13,4 +13,9 @@ p1 = ((casa2[0] - casa1[0]) ** 2 + (casa2[1] - casa1[1]) ** 2)
 
 r = math.sqrt(p1)
 
-print("La distancia entre las casas es de", r, "casas")
+print("La distancia entre las casillas es de", r, "casillas")
+
+if x1 == y1 or x2 == y2 :
+    print("Estas en JaqueMate")
+else :
+    print ("Aun estas a salvo")
