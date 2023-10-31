56ce77d16e83264300a8ff3726114e5ca89d2be7
FernandoAlfonso07 <148348192+FernandoAlfonso07@users.noreply.github.com>
2023-10-31 09:57:43 -0600

    Add files via upload

--- archivo1.py
+++ archivo1.py
@@ -1,5 +1,12 @@
 #VERSION 8_ajedrez - AUTOR : Fernando Alfonso
 
+
+def amenaza_alfil (x1,x2,y1,y2):
+    if abs(x2 - y2) == abs(y1 - x1):
+        return True
+    else :
+        return False
+
 x1 = int(input("Ingrese columna 1 = "))
 x2 = int(input("Ingrese fila 1 = "))
 casa1 = (x1, x2)
@@ -13,9 +20,16 @@ p1 = ((casa2[0] - casa1[0]) ** 2 + (casa2[1] - casa1[1]) ** 2)
 
 r = math.sqrt(p1)
 
-print("La distancia entre las casillas es de", r, "casillas")
+#if x1 == y1 or x2 == y2 :
+    #print("Estas en JaqueMate")
+#else :
+    #print ("Aun estas a salvo")
 
-if x1 == y1 or x2 == y2 :
-    print("Estas en JaqueMate")
+
+if amenaza_alfil (x1,x2,y1,y2)  :
+   print("Estas en JaqueMate")
 else :
     print ("Aun estas a salvo")
+
+
+
